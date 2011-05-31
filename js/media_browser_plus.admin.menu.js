f8cd4029d851510ea3a208d1cfc031d8230c3a00
Peter Philipp <peter.philipp@cando-image.com>
2011-05-31 18:33:46 +0200

    Start with update to media post beta4 - with the new file entity handling *yay*

--- js/media_browser_plus.admin.menu.js
+++ js/media_browser_plus.admin.menu.js
@@ -5,15 +5,16 @@
       if($('ul.action-links').html() == null) {
         $('#media-admin').prepend('<div><ul class="action-links"></ul></div>');
       }
-      var $filterLink = "";
-      if(Drupal.settings.media_browser_plus.filter_active && Drupal.settings.media_browser_plus.filter_allowed)
-        $filterLink = $('<a id="media-filter-launch" href="' + Drupal.settings.media_browser_plus.url + '?q=admin/content/media/filter"></a>').html('Change <b>(active)</b> Filter');
-      else
-        $filterLink = $('<a id="media-filter-launch" href="' + Drupal.settings.media_browser_plus.url + '?q=admin/content/media/filter"></a>').html('Apply Filter');
-      $('ul.action-links', context).append($('<li></li>').append($filterLink));
+      var filterLink = "";
+      if(Drupal.settings.media_browser_plus.filter_active && Drupal.settings.media_browser_plus.filter_allowed){
+        filterLink = $('<a id="media-filter-launch" href="' + Drupal.settings.media_browser_plus.url + '?q=admin/content/media/filter"></a>').html('Change <b>(active)</b> Filter');
+      }
+      else {
+        filterLink = $('<a id="media-filter-launch" href="' + Drupal.settings.media_browser_plus.url + '?q=admin/content/media/filter"></a>').html('Apply Filter');
+      }
+      $('ul.action-links', context).append($('<li></li>').append(filterLink));
       if(Drupal.settings.media_browser_plus.manage_folders) {
-        var $foldersLink = $('<a href="' + Drupal.settings.media_browser_plus.folder_management_url + '"></a>').html('Folder Management');
-        $('ul.action-links', context).append($('<li></li>').append($foldersLink));
+        $('ul.action-links', context).append('<li><a href="' + Drupal.settings.media_browser_plus.folder_management_url + '">' + Drupal.t('Folder Management') + '</a></li>');
       }
       if(!Drupal.settings.media_browser_plus.add_files) {
         $('.media-launcher').remove();
